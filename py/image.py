2ddb0bdb194ad2c8a3c2085a665b2aab168fcf27
cubomx <cheservelram657@gmail.com>
2022-05-03 22:59:38 -0500

    UPDATE: Correctly using uniform distributions. Possibility of having more than 1 point per cell.

--- py/image.py
+++ py/image.py
@@ -3,11 +3,8 @@ from random import randint
 from math import ceil
 import cv2
 
-map = None
-minimio = 100000
 class ImageData:
     def __init__(self, maskSize, cellSize, size=(1024,1024,1), circleSize = 5,imageDensity = 0.02):
-        global map
         self.map = np.zeros(maskSize)
         self.size = size
         self.cellSize = cellSize
@@ -21,22 +18,28 @@ class ImageData:
 
 class Image:
     def randomMatrix(imageData: ImageData):
-        
-        global minimio
+        Map = imageData.map
         # generate a blank image
         img = np.zeros(imageData.size, dtype=np.uint8)
-        # generate a random distribution
-        dist = np.random.uniform(0, imageData.maskSize[0], size=100)
+        # generate random distribution for both axes
+        
+        # init index values to loop around distributions
         circleSize = imageData.circleSize
         cellSize = imageData.cellSize
         # generate a distribution from 0 t0 1 for moving around the cell
         dist_coord = np.random.uniform(0, 1.0, 20)
+        
         for _ in range(imageData.circles):
+            distX = np.random.uniform(0, imageData.maskSize[0], size=imageData.maskSize[0]*4)
+            distY = np.random.uniform(0, imageData.maskSize[1], size=imageData.maskSize[1]*4)
+
+            idX = 0
+            idY = 0
             while True:
                 
                 # get the cell in which we are going to draw the circle
-                randCol = int(dist[randint(0,99)])
-                randRow = int(dist[randint(0,99)])
+                randCol = int(distX[idX])
+                randRow = int(distY[idY])
                 # get the center coords inside the cell
                 X = int( circleSize + dist_coord[randint(0,19)]  * (cellSize[0] - circleSize))
                 Y = int( circleSize + dist_coord[randint(0,19)]  * (cellSize[0] - circleSize))
@@ -45,19 +48,15 @@ class Image:
                 ## height - ( Cell.Height * Cell.Num  / 2)
                 start_point = int( (imageData.size[0] - cellSize[0] * imageData.maskSize[0]) / 2)
                 
-                
+               
                 # draw one circle per cell
-                if imageData.map[randCol][randRow] == 0:
-                    
+                if imageData.map[randCol][randRow] <= imageData.mapMin:
                     # making unavailable this cell
-                    imageData.map[randCol][randRow] = 1
+                    imageData.map[randCol][randRow] += 1
                     # p0 + range(0-24) * 25 + ?
                     x = start_point + randRow * cellSize[0] + X
                     y = start_point + randCol * cellSize[0] + Y
                     
-                    minimio = min(minimio, x)
-                    if imageData.counter > 610:
-                        print(minimio)
                     # draw a circle
                     cv2.circle(img, (x, y), circleSize, 255, -1)
                     imageData.counter += 1
@@ -65,4 +64,11 @@ class Image:
                 # verifying if all cells have been used
                 if imageData.counter >= imageData.maskSize[0]*imageData.maskSize[1]:
                     break
-        return (img, imageData.counter)
+
+                idX += 1
+                idY = idX
+
+                if idX >= imageData.maskSize[0]:
+                    return (Map, None, False)
+
+        return (img, imageData.counter, True)
--- py/main.py
+++ py/main.py
@@ -1,6 +1,9 @@
+from hmac import new
 import cv2
 from utilities import Utilities
 from image import Image, ImageData
+from copy import deepcopy as dp
+import numpy as np
 
 Utilities.clean(True, "images/", True)
 
@@ -10,14 +13,32 @@ myData = ImageData(maskSize=(25,25), cellSize=(20, 20))
 num_circles = myData.maskSize[0] * myData.maskSize[1]
 circles_perImg = myData.circles
 num_Images = 50
+counti = 0
 for i in range(num_Images):
     # check if sufficient missing circles to complete a image
     if num_circles - myData.counter >= circles_perImg:
+         
         randomName =  Utilities.generateRandomString(10)
-        new_image, counter = Image.randomMatrix(myData)
+        # try until success
+        counter = 0
+        new_image = None
+        while True:
+            Map, Counter = myData.map.copy(), dp(myData.counter)
+            new_image, counter, isCorrect = Image.randomMatrix(myData)
+            if isCorrect:
+                break
+            else:
+                myData.map = Map
+                myData.counter = Counter
+        
+        if counter is not None:
+            counti = max(counti, counter)
+        
+        if counter >= num_circles * .95 * (myData.mapMin + 1):
+            myData.mapMin += 1
 
         if i == 49:
-            print(counter)
+            print(counti)
         cv2.imwrite("images/" + randomName + '.png', new_image)
     else:
         break
